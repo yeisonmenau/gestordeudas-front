cf016e353f19004c1a09940bfd7365c7133a60bc
yeisonmenau <yeisonandresdh@gmail.com>
2025-08-15 19:22:16 -0500

    Servicio para el controlador usuario

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UsuarioRequest, UsuarioResponse } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,21 @@ export class Api {
 
     constructor(private http: HttpClient) {}
 
+    crearPersona(usuario:UsuarioRequest): Observable<UsuarioResponse> {
+        return this.http.post<UsuarioResponse>(`${this.apiUrl}/persona`, usuario);
+    }
+    mostrarPersonas(): Observable<UsuarioResponse[]> {
+        return this.http.get<UsuarioResponse[]>(`${this.apiUrl}/persona`);
+    }
+    actualizarPersona(idPersona: number, usuario: UsuarioRequest): Observable<UsuarioResponse> {
+        return this.http.put<UsuarioResponse>(`${this.apiUrl}/persona/${idPersona}`, usuario);
+    }
+    eliminarPersona(idPersona: number): Observable<string> {
+        return this.http.delete<string>(`${this.apiUrl}/persona/${idPersona}`);
+    }
+
+    
+    
     
 
 }
