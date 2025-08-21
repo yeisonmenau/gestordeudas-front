8fc8b0dac2a0569be4cb9b782ccafed30f87cb8a
yeisonmenau <yeisonandresdh@gmail.com>
2025-08-20 22:17:06 -0500

    Servicio para el controlador deuda

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioRequest, UsuarioResponse } from '../models/usuario.model';
+import { DeudaRequest, DeudaResponse } from '../models/deuda.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +25,24 @@ export class Api {
         return this.http.delete<string>(`${this.apiUrl}/persona/${idPersona}`);
     }
 
-    
+    crearDeuda(DeudaRequest: DeudaRequest): Observable<DeudaResponse> {
+        return this.http.post<DeudaResponse>(`${this.apiUrl}/deuda`, DeudaRequest);
+    }
+    mostrarDeudas(): Observable<DeudaResponse[]> {
+        return this.http.get<DeudaResponse[]>(`${this.apiUrl}/deuda`);
+    }
+    actualizarDeuda(idDeuda: number, deuda: DeudaRequest): Observable<DeudaResponse> {
+        return this.http.put<DeudaResponse>(`${this.apiUrl}/deuda/${idDeuda}`, deuda);
+    }
+    eliminarDeuda(idDeuda: number): Observable<string> {
+        return this.http.delete<string>(`${this.apiUrl}/deuda/${idDeuda}`);
+    }
+    saldarDeuda(idDeuda: number): Observable<string> {
+        return this.http.put<string>(`${this.apiUrl}/deuda/saldar/${idDeuda}`, {});
+    }
+    mostrarDeudaPorPersona(idPersona: number): Observable<DeudaResponse[]> {
+        return this.http.get<DeudaResponse[]>(`${this.apiUrl}/deuda/persona/${idPersona}`);
+    }
     
     
 
